587da3b2628cc044931632a183038b074700bb23
mikesch803 <mikesch803@gmail.com>
2020-12-10 11:51:48 +0530

    added js, server, pic, change nav, footer, style

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-var inputText = document.querySelector('#input-txt')
-var translateBtn = document.querySelector('#translate-btn')
-var outputText = document.querySelector('#output-txt')
+var inputText = document.querySelector('#input-txt');
+var translateBtn = document.querySelector('#translate-btn');
+var outputText = document.querySelector('#output-txt');
 
+//server
+var server ="https://api.funtranslations.com/translate/dothraki.json";
+
+//errorhandler
+
+//translatedurl
+function dothrakiURL(text)
+{
+    return server + "?" +"text=" +text;
+}
+
+function clickeventHandler()
+{
+    var input =inputText.value;
+    fetch(dothrakiURL(input))
+        .then (response => response.json())
+        .then (json => {
+            var translatedTxt = json.contents.translated
+            outputText.innerText = translatedTxt
+        })
+    
+};
+
+translateBtn.addEventListener("click", clickeventHandler);
